381e01fbf74a22b3d7e8c078f16d1f2c68e36ed4
GabrielRoger07 <gabrielroger4203@gmail.com>
2025-09-30 19:41:40 -0300

    New version of Company entity

--- backend/src/main/java/com/bakeryquotation/backend/Company/Company.java
+++ backend/src/main/java/com/bakeryquotation/backend/Company/Company.java
@@ -1,11 +1,12 @@
 package com.bakeryquotation.backend.Company;
 
 import jakarta.persistence.*;
+import jakarta.validation.constraints.Email;
 import jakarta.validation.constraints.NotEmpty;
 import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Size;
 import org.hibernate.validator.constraints.br.CNPJ;
 
+import java.time.LocalDateTime;
 import java.util.Objects;
 
 @Entity
@@ -13,20 +14,36 @@ import java.util.Objects;
 public class Company {
     @Id
     @CNPJ
-    @Column(name = "company_cnpj", columnDefinition = "VARCHAR(14)")
+    @Column(name = "companyCnpj", length = 14)
     private String companyCnpj;
 
     @NotNull
     @NotEmpty
-    @Column(name = "company_name", nullable = false, columnDefinition = "VARCHAR(45)")
+    @Column(name = "companyName", nullable = false, length = 45)
     private String companyName;
+
+    @NotNull
+    @NotEmpty
+    @Column(name = "companyWhatsappNumber", nullable = false, length = 16)
+    private String companyWhatsappNumber;
+
+    @NotNull
+    @NotEmpty
+    @Email
+    @Column(name = "companyEmail", nullable = false, length = 60)
+    private String companyEmail;
+
+    @Column(name = "createdAt", nullable = false, columnDefinition = "DATETIME", insertable = false, updatable = false)
+    private LocalDateTime createdAt;
 
     public Company() {
     }
 
-    public Company(String companyCnpj, String companyName) {
+    public Company(String companyCnpj, String companyName, String companyWhatsappNumber, String companyEmail) {
         this.companyCnpj = companyCnpj;
         this.companyName = companyName;
+        this.companyWhatsappNumber = companyWhatsappNumber;
+        this.companyEmail = companyEmail;
     }
 
     public String getCompanyCnpj() {
@@ -45,23 +62,39 @@ public class Company {
         this.companyName = companyName;
     }
 
+    public String getCompanyWhatsappNumber() {
+        return companyWhatsappNumber;
+    }
+
+    public void setCompanyWhatsappNumber(String companyWhatsappNumber) {
+        this.companyWhatsappNumber = companyWhatsappNumber;
+    }
+
+    public String getCompanyEmail() {
+        return companyEmail;
+    }
+
+    public void setCompanyEmail(String companyEmail) {
+        this.companyEmail = companyEmail;
+    }
+
+    public LocalDateTime getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(LocalDateTime createdAt) {
+        this.createdAt = createdAt;
+    }
+
     @Override
     public boolean equals(Object o) {
         if (o == null || getClass() != o.getClass()) return false;
         Company company = (Company) o;
-        return Objects.equals(companyCnpj, company.companyCnpj) && Objects.equals(companyName, company.companyName);
+        return Objects.equals(companyCnpj, company.companyCnpj) && Objects.equals(companyName, company.companyName) && Objects.equals(companyWhatsappNumber, company.companyWhatsappNumber) && Objects.equals(companyEmail, company.companyEmail) && Objects.equals(createdAt, company.createdAt);
     }
 
     @Override
     public int hashCode() {
-        return Objects.hash(companyCnpj, companyName);
-    }
-
-    @Override
-    public String toString() {
-        return "Company{" +
-                "companyCnpj='" + companyCnpj + '\'' +
-                ", companyName='" + companyName + '\'' +
-                '}';
+        return Objects.hash(companyCnpj, companyName, companyWhatsappNumber, companyEmail, createdAt);
     }
 }

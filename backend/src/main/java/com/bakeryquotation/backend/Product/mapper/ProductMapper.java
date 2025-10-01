9b591706a22a43b9324f440a47201fd39dfa8037
GabrielRoger07 <gabrielroger4203@gmail.com>
2025-10-01 13:49:40 -0300

    Product mapper created

---
+++ backend/src/main/java/com/bakeryquotation/backend/Product/mapper/ProductMapper.java
@@ -0,0 +1,17 @@
+package com.bakeryquotation.backend.Product.mapper;
+
+import com.bakeryquotation.backend.Product.Product;
+import com.bakeryquotation.backend.Product.ProductRequestDTO;
+import com.bakeryquotation.backend.Product.ProductResponseDTO;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface ProductMapper {
+
+    @Mapping(target = "id", ignore = true)
+    Product toEntity(ProductRequestDTO productRequestDTO);
+
+    @Mapping(source = "id", target = "productId")
+    ProductResponseDTO toDto(Product product);
+}

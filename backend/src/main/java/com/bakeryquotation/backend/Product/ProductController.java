2e548abc2751bcd3715b9e6af9e0de57900fa49a
GabrielRoger07 <gabrielroger4203@gmail.com>
2025-10-01 13:58:33 -0300

    Product controller created

---
+++ backend/src/main/java/com/bakeryquotation/backend/Product/ProductController.java
@@ -0,0 +1,35 @@
+package com.bakeryquotation.backend.Product;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/products")
+public class ProductController {
+
+    private final ProductService productService;
+
+    public ProductController(ProductService productService){
+        this.productService = productService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<ProductResponseDTO>> getAllProducts(){
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<ProductResponseDTO> getProductById(Long id){
+        return productService.getProductById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<ProductResponseDTO> deleteProductById(Long id){
+        return productService.deleteProductById(id);
+    }
+}

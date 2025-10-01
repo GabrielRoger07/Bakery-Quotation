6644f26f37f7108723b678cac76c2aeb53f796eb
GabrielRoger07 <gabrielroger4203@gmail.com>
2025-10-01 13:56:03 -0300

    Product service created

---
+++ backend/src/main/java/com/bakeryquotation/backend/Product/ProductService.java
@@ -0,0 +1,41 @@
+package com.bakeryquotation.backend.Product;
+
+import com.bakeryquotation.backend.Product.mapper.ProductMapper;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class ProductService {
+
+    private final ProductRepository productRepository;
+    private final ProductMapper productMapper;
+
+    public ProductService(ProductRepository productRepository, ProductMapper productMapper){
+        this.productRepository = productRepository;
+        this.productMapper = productMapper;
+    }
+
+    public ResponseEntity<List<ProductResponseDTO>> getAllProducts(){
+        List<Product> products = productRepository.findAll();
+        List<ProductResponseDTO> productResponseDTOS = new ArrayList<>();
+        products.forEach(product -> {
+            productResponseDTOS.add(productMapper.toDto(product));
+        });
+        return ResponseEntity.status(HttpStatus.OK).body(productResponseDTOS);
+    }
+
+    public ResponseEntity<ProductResponseDTO> getProductById(Long id){
+        Product product = productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product with id " + id + " does not exists"));
+        return ResponseEntity.status(HttpStatus.OK).body(productMapper.toDto(product));
+    }
+
+    public ResponseEntity<ProductResponseDTO> deleteProductById(Long id){
+        Product product = productRepository.findById(id).orElseThrow(() -> new RuntimeException("Product with id " + id + " does not exists"));
+        productRepository.delete(product);
+        return ResponseEntity.status(HttpStatus.OK).body(productMapper.toDto(product));
+    }
+}

8199b823a6106c3b9894d25430e858d5a5804e97
GabrielRoger07 <gabrielroger4203@gmail.com>
2025-10-01 18:50:42 -0300

    Login page created

---
+++ frontend/src/components/Alert.jsx
@@ -0,0 +1,10 @@
+import React from 'react'
+
+const Alert = ({ message }) => {
+    if(!message) return null;
+  return (
+    <div className="alert">{message}</div>
+  )
+}
+
+export default Alert
---
+++ frontend/src/pages/Login.jsx
@@ -0,0 +1,42 @@
+import React, { useState } from 'react'
+import Input from '../components/Input'
+import Alert from '../components/Alert'
+import Button from '../components/Button'
+
+const Login = () => {
+    const [email, setEmail] = useState("")
+    const [password, setPassword] = useState("")
+    const [error, setError] = useState("")
+
+    const handleLogin = (e) => {
+        e.preventDefault();
+
+        if(!email || !password){
+            setError("Please fill in all fields");
+            return;
+        }
+        if (!/\S+@\S+\.\S+/.test(email)) {
+            setError("Invalid email address");
+            return;
+        }
+
+        setError("")
+        //chamada da api
+        console.log({ email, password })
+    }
+
+    return (
+        <div className='login-container'>
+            <h1>Pani Premium Login</h1>
+            <form onSubmit={handleLogin}>
+                <Input label="Email" type="email" value={email} onChange={(e) => setEmail(e.target.value)} placeholder="Enter your email" />
+                <Input label="Password" type="password" value={password} onChange={(e) => setPassword(e.target.value)} placeholder="Enter your password" />
+                <Alert message={error} />
+                <Button type="submit">Login</Button>
+            </form>
+            <p><a href="#">Forgot your password?</a></p>
+        </div>
+    )
+}
+
+export default Login

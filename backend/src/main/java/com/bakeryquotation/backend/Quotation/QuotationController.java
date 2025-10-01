4aa4c832f76656ff231c8bc69e931a33b0f5bdd8
GabrielRoger07 <gabrielroger4203@gmail.com>
2025-10-01 14:10:49 -0300

    Quotation controller created

---
+++ backend/src/main/java/com/bakeryquotation/backend/Quotation/QuotationController.java
@@ -0,0 +1,32 @@
+package com.bakeryquotation.backend.Quotation;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/api/v1/quotations")
+public class QuotationController {
+
+    private final QuotationService quotationService;
+
+    public QuotationController(QuotationService quotationService){
+        this.quotationService = quotationService;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<QuotationResponseDTO>> getAllQuotations(){
+        return quotationService.getAllQuotations();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<QuotationResponseDTO> getQuotationById(@PathVariable("id") Long id){
+        return quotationService.getQuotationById(id);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<QuotationResponseDTO> deleteQuotationById(@PathVariable("id") Long id){
+        return quotationService.deleteQuotationById(id);
+    }
+}

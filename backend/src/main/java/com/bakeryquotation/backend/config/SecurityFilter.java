1ec51a1e814797cf2aa4cce28df6083247ce8cc6
GabrielRoger07 <gabrielroger4203@gmail.com>
2025-10-12 15:05:16 -0300

    Authentication filter created

---
+++ backend/src/main/java/com/bakeryquotation/backend/config/SecurityFilter.java
@@ -0,0 +1,40 @@
+package com.bakeryquotation.backend.config;
+
+import com.bakeryquotation.backend.Company.CompanyRepository;
+import jakarta.servlet.FilterChain;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.stereotype.Component;
+import org.springframework.web.filter.OncePerRequestFilter;
+
+import java.io.IOException;
+
+@Component
+public class SecurityFilter extends OncePerRequestFilter {
+
+    private final TokenConfig tokenConfig;
+    private final CompanyRepository companyRepository;
+
+    public SecurityFilter(TokenConfig tokenConfig, CompanyRepository companyRepository){
+        this.tokenConfig = tokenConfig;
+        this.companyRepository = companyRepository;
+    }
+
+    @Override
+    protected void doFilterInternal(HttpServletRequest request, HttpServletResponse response, FilterChain filterChain) throws ServletException, IOException {
+        String authorizedHeader = request.getHeader("Authorization");
+        if(authorizedHeader != null && authorizedHeader.startsWith("Bearer ")){
+            String token = authorizedHeader.replace("Bearer ", "");
+            String subject = tokenConfig.validateToken(token);
+            UserDetails company = companyRepository.findByCompanyEmail(subject);
+
+            UsernamePasswordAuthenticationToken authenticationToken = new UsernamePasswordAuthenticationToken(company, null, company.getAuthorities());
+            SecurityContextHolder.getContext().setAuthentication(authenticationToken);
+        }
+        filterChain.doFilter(request, response);
+    }
+}

c69fd71014d0af20e09839ef6ccd4d13b5b841a3
GabrielRoger07 <gabrielroger4203@gmail.com>
2025-10-12 15:03:39 -0300

    UserDetailsService implementation created

---
+++ backend/src/main/java/com/bakeryquotation/backend/config/AuthConfig.java
@@ -0,0 +1,22 @@
+package com.bakeryquotation.backend.config;
+
+import com.bakeryquotation.backend.Company.CompanyRepository;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class AuthConfig implements UserDetailsService {
+
+    private final CompanyRepository companyRepository;
+
+    public AuthConfig(CompanyRepository companyRepository){
+        this.companyRepository = companyRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String companyEmail) throws UsernameNotFoundException {
+        return companyRepository.findByCompanyEmail(companyEmail);
+    }
+}

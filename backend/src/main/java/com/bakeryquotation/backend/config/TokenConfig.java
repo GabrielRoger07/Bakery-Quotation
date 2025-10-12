0497c8db386781b74ede47f54833dbbae3cd5535
GabrielRoger07 <gabrielroger4203@gmail.com>
2025-10-12 15:04:29 -0300

    JWT token configuration created

---
+++ backend/src/main/java/com/bakeryquotation/backend/config/TokenConfig.java
@@ -0,0 +1,40 @@
+package com.bakeryquotation.backend.config;
+
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTVerificationException;
+import com.bakeryquotation.backend.Company.Company;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.time.Instant;
+
+@Component
+public class TokenConfig {
+
+    @Value("${api.security.token.secret}")
+    private String secret;
+
+    public String generateToken(Company company){
+
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        return JWT.create()
+                .withSubject(company.getCompanyEmail())
+                .withClaim("companyCnpj", company.getCompanyCnpj())
+                .withExpiresAt(Instant.now().plusSeconds(86400))
+                .withIssuedAt(Instant.now())
+                .sign(algorithm);
+    }
+
+    public String validateToken(String token){
+
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        try{
+            return JWT.require(algorithm).build().verify(token).getSubject();
+        }catch(JWTVerificationException exception){
+            return "";
+        }
+    }
+}

eeb1829a643b91ffbeaccd69366f7560e294dc1b
GabrielRoger07 <gabrielroger4203@gmail.com>
2025-11-26 10:25:46 -0300

    Add translation on login page

--- frontend/src/pages/Company/Login.jsx
+++ frontend/src/pages/Company/Login.jsx
@@ -1,14 +1,19 @@
 import { useState } from 'react'
 import { Link, useNavigate } from 'react-router-dom'
 import Cookies from 'js-cookie'
+import { useTranslation } from 'react-i18next'
 import useFetch from '../../hooks/useFetch'
 import Input from '../../components/Input'
 import Button from '../../components/Button'
 import Alert from '../../components/Alert'
 import '../../components/Auth.css'
 import { ENV } from '../../config/env'
+import PublicHeader from '../../components/PublicHeader'
 
 const Login = () => {
+
+    const { t } = useTranslation()
+    
     const [companyEmail, setCompanyEmail] = useState("")
     const [companyPassword, setCompanyPassword] = useState("")
     const [error, setError] = useState("")
@@ -21,11 +26,11 @@ const Login = () => {
         e.preventDefault();
 
         if(!companyEmail || !companyPassword){
-            setError("Please fill in all fields");
+            setError(t("fill_all_fields"));
             return;
         }
         if (!/\S+@\S+\.\S+/.test(companyEmail)) {
-            setError("Invalid email address");
+            setError(t("invalid_email"));
             return;
         }
 
@@ -39,33 +44,35 @@ const Login = () => {
         const res = await request("POST", "/companies/login", login)
 
         if(res.ok){
-            setSuccess("Login successfully!")
+            setSuccess(t("login_success"))
             setError("")
             const token = res.data.token
             Cookies.set("token", token)
             setTimeout(() => navigate("/suppliers"), 1000)
         }else{
             setSuccess("")
-            setError(res.data?.message)
+            console.log(res.data)
+            setError(t("login_error"))
         }
     }
 
     return (
         <div className='auth-container'>
+            <PublicHeader />
             <div className='auth-box'>
-                <h1>Login</h1>
+                <h1>{t("login")}</h1>
                 <form onSubmit={handleLogin}>
-                    <Input label="Email" type="email" name="companyEmail" value={companyEmail} onChange={(e) => setCompanyEmail(e.target.value)} placeholder="Enter your email" />
-                    <Input label="Password" type="password" name="companyPassword" value={companyPassword} onChange={(e) => setCompanyPassword(e.target.value)} placeholder="Enter your password" />
+                    <Input label={t("email")} type="email" name="companyEmail" value={companyEmail} onChange={(e) => setCompanyEmail(e.target.value)} placeholder={t("enter_email")} />
+                    <Input label={t("password")} type="password" name="companyPassword" value={companyPassword} onChange={(e) => setCompanyPassword(e.target.value)} placeholder={t("enter_password")} />
                     <Alert message={error} />
                     {success && <div className="success">{success}</div>}
-                    <Button type="submit" loading={loading}>Login</Button>
+                    <Button type="submit" loading={loading}>{t("login")}</Button>
                 </form>
                 {/*
                 <p><a href="#">Forgot your password?</a></p>
                 */}
                 <p>
-                    <Link to="/register">New company? Create an account!</Link>
+                    <Link to="/register">{t("new_company")}</Link>
                 </p>
             </div>
         </div>

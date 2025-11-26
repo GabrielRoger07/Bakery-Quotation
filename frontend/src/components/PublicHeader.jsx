7bbcfcbacd2eba74eb55a34933590127c8072b95
GabrielRoger07 <gabrielroger4203@gmail.com>
2025-11-26 10:24:51 -0300

    Add header for public pages

---
+++ frontend/src/components/PublicHeader.jsx
@@ -0,0 +1,12 @@
+import LangSwitcher from './LangSwitcher'
+import './PublicHeader.css'
+
+const PublicHeader = () => {
+    return (
+        <header className='public-header'>
+            <LangSwitcher />
+        </header>
+    )
+}
+
+export default PublicHeader

bda739b97a52fabf229a66f6b5a8f144372b1065
GabrielRoger07 <gabrielroger4203@gmail.com>
2025-11-26 18:38:05 -0300

    Add portuguese translation on table component

--- frontend/src/components/Table.jsx
+++ frontend/src/components/Table.jsx
@@ -1,70 +1,74 @@
 import React from 'react'
 import './Table.css'
+import { useTranslation } from 'react-i18next'
 import Button from './Button'
 import { Pencil, Trash, Eye, Activity } from 'lucide-react'
 
 const Table = ({title, columns = [], data = [], idKey = "id", loading = false, emptyMessage = "No records found.", onEdit, onDelete, onAdd, onView, onReload, onMonitor, onSort, sortField, sortDirection }) => {
-  return (
-    <div className="table-container">
-        <div className="table-header">
-            <h1>{title}</h1>
-            <div className="table-header-buttons">
-                {loading && <div className="loading-spinner"></div>}
-                {onReload && <Button onClick={onReload}>Reload</Button>}
-                {onAdd && <Button onClick={onAdd}>Add</Button>}
+  
+    const { t } = useTranslation()
+  
+    return (
+        <div className="table-container">
+            <div className="table-header">
+                <h1>{title}</h1>
+                <div className="table-header-buttons">
+                    {loading && <div className="loading-spinner"></div>}
+                    {onReload && <Button onClick={onReload}>{t("table_reload")}</Button>}
+                    {onAdd && <Button onClick={onAdd}>{t("table_add")}</Button>}
+                </div>
             </div>
-        </div>
 
-        {loading && <p>Loading...</p>}
+            {loading && <p>Loading...</p>}
 
-        {!loading && data.length === 0 &&(
-            <p className="empty-message">{emptyMessage}</p>
-        )}
+            {!loading && data.length === 0 &&(
+                <p className="empty-message">{emptyMessage}</p>
+            )}
 
-        {!loading && data.length > 0 &&(
-            <div className="table-wrapper">
-                <table className="custom-table">
-                    <thead>
-                        <tr>
-                            {columns.map((col) => (
-                                <th key={col.key} onClick={() => onSort && onSort(col.key)} className="sortable-column">{col.label} {sortField === col.key && (
-                                    <span className="sort-indicator">{sortDirection == "asc" ? "▲" : "▼"}</span>
-                                )}</th>
-                            ))}
-                            {(onEdit || onDelete || onView || onMonitor) && <th>Actions</th>}
-                        </tr>
-                    </thead>
-                    <tbody>
-                        {data.map((item, index) => (
-                            <tr key={item[idKey] || index}>
+            {!loading && data.length > 0 &&(
+                <div className="table-wrapper">
+                    <table className="custom-table">
+                        <thead>
+                            <tr>
                                 {columns.map((col) => (
-                                    <td key={col.key}>{item[col.key]}</td>
+                                    <th key={col.key} onClick={() => onSort && onSort(col.key)} className="sortable-column">{col.label} {sortField === col.key && (
+                                        <span className="sort-indicator">{sortDirection == "asc" ? "▲" : "▼"}</span>
+                                    )}</th>
                                 ))}
-                                {(onEdit || onDelete || onView || onMonitor) && (
-                                    <td className="actions">
-                                        {onEdit && (
-                                            <Button onClick={() => onEdit(item)}><Pencil size={18}/></Button>
-                                        )}
-                                        {onDelete && (
-                                            <Button onClick={() => onDelete(item[idKey])}><Trash size={18}/></Button>
-                                        )}
-                                        {onView && (
-                                            <Button onClick={() => onView(item)}><Eye size={18}/></Button>
-                                        )}
-                                        {onMonitor && (
-                                            <Button onClick={() => onMonitor(item)}><Activity size={18}/></Button>
-                                        )}
-                                    </td>
-
-                                )}
+                                {(onEdit || onDelete || onView || onMonitor) && <th>{t("table_actions")}</th>}
                             </tr>
-                        ))}
-                    </tbody>
-                </table>
-            </div>
-        )}
-    </div>
-  )
+                        </thead>
+                        <tbody>
+                            {data.map((item, index) => (
+                                <tr key={item[idKey] || index}>
+                                    {columns.map((col) => (
+                                        <td key={col.key}>{item[col.key]}</td>
+                                    ))}
+                                    {(onEdit || onDelete || onView || onMonitor) && (
+                                        <td className="actions">
+                                            {onEdit && (
+                                                <Button onClick={() => onEdit(item)}><Pencil size={18}/></Button>
+                                            )}
+                                            {onDelete && (
+                                                <Button onClick={() => onDelete(item[idKey])}><Trash size={18}/></Button>
+                                            )}
+                                            {onView && (
+                                                <Button onClick={() => onView(item)}><Eye size={18}/></Button>
+                                            )}
+                                            {onMonitor && (
+                                                <Button onClick={() => onMonitor(item)}><Activity size={18}/></Button>
+                                            )}
+                                        </td>
+
+                                    )}
+                                </tr>
+                            ))}
+                        </tbody>
+                    </table>
+                </div>
+            )}
+        </div>
+    )
 }
 
 export default Table

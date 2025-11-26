27a4479dc3f234ccfb36d2d95397166b77ae37c4
GabrielRoger07 <gabrielroger4203@gmail.com>
2025-11-26 10:24:20 -0300

    Add translation component

---
+++ frontend/src/components/LangSwitcher.jsx
@@ -0,0 +1,42 @@
+import { useTranslation } from 'react-i18next'
+import { useEffect, useRef, useState } from 'react'
+import './LangSwitcher.css'
+
+const LangSwitcher = () => {
+    const { i18n } = useTranslation()
+    const [open, setOpen] = useState(false)
+    const dropdownRef = useRef(null)
+
+    const changeLang = (lang) => {
+        i18n.changeLanguage(lang)
+        setOpen(false)
+    }
+
+    useEffect(() => {
+        const handleClickOutside = (event) => {
+            if(dropdownRef.current && !dropdownRef.current.contains(event.target)){
+                setOpen(false)
+            }
+        }
+
+        document.addEventListener("mousedown", handleClickOutside)
+
+        return () => {
+            document.removeEventListener("mousedown", handleClickOutside)
+        }
+    }, [])
+
+    return (
+        <div className='lang-dropdown' ref={dropdownRef}>
+            <button className="lang-trigger" onClick={() => setOpen(!open)}>🌐</button>
+            {open && (
+                <div className='lang-menu'>
+                    <button className={i18n.language === "en" ? "active" : ""} onClick={() => i18n.changeLanguage("en")}>🇺🇸 EN</button>
+                    <button className={i18n.language === "pt" ? "active" : ""} onClick={() => i18n.changeLanguage("pt")}>🇧🇷 PT</button>
+                </div>
+            )}
+        </div>
+    )
+}
+
+export default LangSwitcher

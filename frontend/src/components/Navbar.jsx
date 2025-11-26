727a2e2fe1f23c600eb5a00864071ea303b4aad4
GabrielRoger07 <gabrielroger4203@gmail.com>
2025-11-26 18:34:52 -0300

    Add portuguese translation on private navbar

--- frontend/src/components/Navbar.jsx
+++ frontend/src/components/Navbar.jsx
@@ -1,10 +1,13 @@
 import { NavLink, useNavigate } from "react-router-dom";
 import Cookies from "js-cookie";
+import { useTranslation } from "react-i18next";
 import Button from "./Button";
 import LangSwitcher from "./LangSwitcher";
 import './Navbar.css'
 
 const Navbar = () => {
+
+    const { t } = useTranslation()
 
     const navigate = useNavigate();
 
@@ -16,13 +19,13 @@ const Navbar = () => {
     return (
         <nav className="navbar">
             <div className="navbar-center">
-                <NavLink to="/suppliers">Suppliers</NavLink>
-                <NavLink to="/products">Products</NavLink>
-                <NavLink to="/quotations">Quotations</NavLink>
+                <NavLink to="/suppliers">{t("navbar_suppliers")}</NavLink>
+                <NavLink to="/products">{t("navbar_products")}</NavLink>
+                <NavLink to="/quotations">{t("navbar_quotations")}</NavLink>
             </div>
             <div className="navbar-right">
                 <LangSwitcher />
-                <Button onClick={logout}> Logout</Button>
+                <Button onClick={logout}>{t("navbar_logout")}</Button>
             </div>
         </nav>
     )

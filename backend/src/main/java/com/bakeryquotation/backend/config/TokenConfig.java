ca5a6562a1f69380f4bc89bba78e292bc3016e0f
GabrielRoger07 <gabrielroger4203@gmail.com>
2026-03-16 23:32:52 -0300

    Add new jwt methods

--- backend/src/main/java/com/bakeryquotation/backend/config/TokenConfig.java
+++ backend/src/main/java/com/bakeryquotation/backend/config/TokenConfig.java
@@ -3,8 +3,8 @@ package com.bakeryquotation.backend.config;
 import com.auth0.jwt.JWT;
 import com.auth0.jwt.algorithms.Algorithm;
 import com.auth0.jwt.exceptions.JWTVerificationException;
-import com.auth0.jwt.interfaces.DecodedJWT;
 import com.bakeryquotation.backend.Company.Company;
+import com.bakeryquotation.backend.Supplier.Supplier;
 import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Component;
 
@@ -16,11 +16,17 @@ public class TokenConfig {
     @Value("${api.security.token.secret}")
     private String secret;
 
-    @Value("${api.security.token.expiration}")
-    private Long expiration;
+    @Value("${api.security.token.company.expiration}")
+    private Long companyTokenExpiration;
 
-    @Value("${api.security.token.refresh-token.expiration}")
-    private Long refreshTokenExpiration;
+    @Value("${api.security.token.company.refresh-token.expiration}")
+    private Long companyRefreshTokenExpiration;
+
+    @Value("${api.security.token.supplier.expiration}")
+    private Long supplierTokenExpiration;
+
+    @Value("${api.security.token.supplier.refresh-token.expiration}")
+    private Long supplierRefreshTokenExpiration;
 
     public String generateToken(Company company){
 
@@ -29,7 +35,8 @@ public class TokenConfig {
         return JWT.create()
                 .withSubject(company.getCompanyEmail())
                 .withClaim("companyCnpj", company.getCompanyCnpj())
-                .withExpiresAt(Instant.now().plusSeconds(expiration))
+                .withClaim("userType", "COMPANY")
+                .withExpiresAt(Instant.now().plusSeconds(companyTokenExpiration))
                 .withIssuedAt(Instant.now())
                 .sign(algorithm);
     }
@@ -41,7 +48,32 @@ public class TokenConfig {
         return JWT.create()
                 .withSubject(company.getCompanyEmail())
                 .withClaim("companyCnpj", company.getCompanyCnpj())
-                .withExpiresAt(Instant.now().plusSeconds(refreshTokenExpiration))
+                .withClaim("userType", "COMPANY")
+                .withExpiresAt(Instant.now().plusSeconds(companyRefreshTokenExpiration))
+                .withIssuedAt(Instant.now())
+                .sign(algorithm);
+    }
+
+    public String generateSupplierToken(Supplier supplier){
+
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        return JWT.create()
+                .withSubject(supplier.getId().toString())
+                .withClaim("userType", "SUPPLIER")
+                .withExpiresAt(Instant.now().plusSeconds(supplierTokenExpiration))
+                .withIssuedAt(Instant.now())
+                .sign(algorithm);
+    }
+
+    public String generateSupplierRefreshToken(Supplier supplier){
+
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        return JWT.create()
+                .withSubject(supplier.getId().toString())
+                .withClaim("userType", "SUPPLIER")
+                .withExpiresAt(Instant.now().plusSeconds(supplierRefreshTokenExpiration))
                 .withIssuedAt(Instant.now())
                 .sign(algorithm);
     }
@@ -55,6 +87,21 @@ public class TokenConfig {
                     .build()
                     .verify(accessToken)
                     .getSubject();
+        }catch(JWTVerificationException exception){
+            return null;
+        }
+    }
+
+    public String getUserTypeFromToken(String accessToken){
+
+        Algorithm algorithm = Algorithm.HMAC256(secret);
+
+        try{
+            return JWT.require(algorithm)
+                    .build()
+                    .verify(accessToken)
+                    .getClaim("userType")
+                    .asString();
         }catch(JWTVerificationException exception){
             return null;
         }

bcba67a64dfe9330bea3189e9d7f8b4623ccd4e6
GabrielRoger07 <gabrielroger4203@gmail.com>
2026-06-26 00:54:38 -0300

    Add clsx and tailwind-merge

--- frontend/src/pages/Quotation/QuotationCreate.jsx
+++
@@ -1,9 +0,0 @@
-import QuotationForm from '@/pages/Quotation/QuotationForm'
-
-const QuotationCreate = ({ onClose, onSave }) => {
-    return (
-        <QuotationForm mode="create" onClose={onClose} onSave={onSave} />
-    )
-}
-
-export default QuotationCreate
